f54caa2d0f2f000538c9e751a5902f013a953f8f
DemonIngester <132798470+DemonIngester@users.noreply.github.com>
2024-01-21 15:30:45 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function textGameP() {
+    open("https://github.com/DemonIngester/ExorcistGame", "_blank")
+}
+
+function uiDesignP() {
+    open("https://github.com/DemonIngester/UI-UX-Designs", "_blank")
+}
+
+function openglHouseP() {
+    open("https://github.com/DemonIngester/OpenGLHouse", "_blank")
+}
+
+function planetExpressP() {
+    open("https://demoningester.github.io/FuturamaProject/", "_blank")
+}
